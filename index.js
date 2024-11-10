cae9ec4d19793a7c0d142fb3eef50fe2fe7d603a
Ryan <ryanvahl@gmail.com>
2024-11-09 23:48:08 -0500

    installed express, created server and initialized package.json

--- index.js
+++ index.js
@@ -2,3 +2,10 @@
 const express = require('express');
 // create instance of express
 const app = express();
+// can get port number later by using variable
+const PORT = 3000;
+
+//create instance of server
+app.listen(PORT, () => {
+    console.log("Server is running");
+});
