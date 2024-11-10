8ceac516942692c5f38168768b25842f41856853
Ryan <ryanvahl@gmail.com>
2024-11-10 00:09:38 -0500

    add basic GET route with some details about it

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const app = express();
 // can get port number later by using variable
 const PORT = 3000;
 
+// request object (req) and response object (res) contains useful information about the request and response respectively
+app.get('/', (req,res) => {
+    // provide a response to client to render
+    res.send("Welcome");
+    console.log(req);
+});
+
 //create instance of server
 app.listen(PORT, () => {
     console.log("Server is running");
