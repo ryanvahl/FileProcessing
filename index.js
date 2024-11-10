e299f78aba7b4bc8a5e766540c07e21b67d4a756
Ryan <ryanvahl@gmail.com>
2024-11-10 04:50:22 -0500

    added uploads folder to store files, added file upload library and ability for /upload post method to save file locally

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
 const express = require('express');
 // create instance of express
 const app = express();
+const fileUpload = require('express-fileupload');
 // can get port number later by using variable
 const PORT = 3000;
+
+//
+app.use(fileUpload());
 
 // the '/' is the URL path the get method will respond to
 // request object (req) and response object (res) contains useful information about the request and response respectively
@@ -17,7 +21,27 @@ app.get('/', (req,res) => {
 // first argument to post is the URL path that needs to have a post request to
 // '/upload' is the route you made for the client to use
 app.post('/upload', (req,res) => {
-    res.send(req.method + " request sent.")
+    // upload path
+    const uploadedFile = req.files.uploadFile;
+
+    // check file
+    console.log(uploadedFile);
+
+    // upload path
+    const uploadPath = __dirname + "\\uploads\\" + uploadedFile.name;
+
+    // check path
+    console.log(uploadPath);
+
+    // use provided path to store file locally. Here is where database would come into play
+    uploadedFile.mv(uploadPath, (err) => {
+        if (err) {
+            console.log(err);
+            res.send("Failed to upload");
+        } else {
+            res.send("Successfully uploaded!");
+        }
+    });
 });
 
 //create instance of server
